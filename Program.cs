122e72cad2a0a17909dd7046afe5f9dcf2a16901
ViniciusQuirino <viniciusquirino13@hotmail.com>
2024-06-25 17:30:57 -0300

    hashSet

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashSet<string> set = new HashSet<string>();
+        set.Add("TV");
+        set.Add("Notebook");
+        set.Add("Tablet");
+        Console.WriteLine(set.Contains("Computer"));
+        foreach (String p in set)
+        {
+            Console.WriteLine(p);
+        }
+    }
+}
