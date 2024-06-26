5fe9cb3c587388b7fc6e9e9945d41b1216d9ab19
ViniciusQuirino <viniciusquirino13@hotmail.com>
2024-06-26 15:53:25 -0300

    sortedSet

--- Program.cs
+++ Program.cs
@@ -5,14 +5,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        HashSet<string> set = new HashSet<string>();
-        set.Add("TV");
-        set.Add("Notebook");
-        set.Add("Tablet");
-        Console.WriteLine(set.Contains("Computer"));
-        foreach (String p in set)
+        SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 6, 8, 10 };
+        SortedSet<int> b = new SortedSet<int>() { 5, 7, 9, 6, 8 };
+
+        //union
+        SortedSet<int> c = new SortedSet<int>(a);
+        // c.UnionWith(b);
+        printCollection(c);
+
+        //difference
+        SortedSet<int> e = new SortedSet<int>(a);
+        e.ExceptWith(b);
+        printCollection(e);
+
+        //intersectiond
+        SortedSet<int> d = new SortedSet<int>(a);
+        d.IntersectWith(b);
+        printCollection(d);
+
+    }
+
+    static void printCollection<T>(IEnumerable<T> collection)
+    {
+        foreach (T obj in collection)
         {
-            Console.WriteLine(p);
+            Console.Write(obj + " ");
         }
+        Console.WriteLine();
     }
 }
